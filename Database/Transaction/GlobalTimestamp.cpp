e833920ccb9e5f0cde6913d357f97e542f4b83ed
Yingjun Wu <wu.yj0616@gmail.com>
2015-11-28 20:15:12 +0800

    refactor contents.

--- Database/Transaction/GlobalTimestamp.cpp
+++ Database/Transaction/GlobalTimestamp.cpp
@@ -2,9 +2,9 @@
 
 namespace Cavalia{
 	namespace Database{
-		std::atomic<uint64_t> GlobalContent::monotone_timestamp_(1);
+		std::atomic<uint64_t> GlobalTimestamp::monotone_timestamp_(1);
 
-		std::atomic<uint64_t> *GlobalContent::thread_timestamp_[kMaxThreadNum];
-		size_t GlobalContent::thread_count_ = 0;
+		std::atomic<uint64_t> *GlobalTimestamp::thread_timestamp_[kMaxThreadNum];
+		size_t GlobalTimestamp::thread_count_ = 0;
 	}
 }
