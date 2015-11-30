621be94510c6bb8870e3acedddb5014b4e0cd746
Yingjun Wu <wu.yj0616@gmail.com>
2015-11-30 20:54:10 +0800

    change to c-style file system api.

--- Database/Logger/BaseLogger.h
+++ Database/Logger/BaseLogger.h
@@ -3,31 +3,38 @@
 #define __CAVALIA_DATABASE_BASE_LOGGER_H__
 
 #include <string>
-#include <fstream>
+#include <cstdio>
 
 namespace Cavalia{
 	namespace Database{
 		class BaseLogger{
 		public:
 			BaseLogger(const std::string &dir_name, const size_t &thread_count, bool is_vl) : dir_name_(dir_name), thread_count_(thread_count){
-				outfiles_ = new std::ofstream[thread_count_];
+				outfiles_ = new FILE*[thread_count_];
 				// is value logging
 				if (is_vl == true){
 					for (size_t i = 0; i < thread_count_; ++i){
-						outfiles_[i].open(dir_name_ + "/vl" + std::to_string(i), std::ofstream::binary);
+						outfiles_[i] = fopen((dir_name_ + "/vl" + std::to_string(i)).c_str(), "wb");
 					}
 				}
 				// is command logging
 				else{
 					for (size_t i = 0; i < thread_count_; ++i){
-						outfiles_[i].open(dir_name_ + "/cl" + std::to_string(i), std::ofstream::binary);
+						outfiles_[i] = fopen((dir_name_ + "/cl" + std::to_string(i)).c_str(), "wb");
 					}
 				}
 			}
 			virtual ~BaseLogger(){
 				for (size_t i = 0; i < thread_count_; ++i){
-					outfiles_[i].flush();
-					outfiles_[i].close();
+					int ret;
+					ret = fflush(outfiles_[i]);
+					assert(ret == 0);
+#if defined(__linux__)
+					ret = fsync(fileno(outfiles_[i]));
+					assert(ret == 0);
+#endif
+					ret = fclose(outfiles_[i]);
+					assert(ret == 0);
 				}
 				delete[] outfiles_;
 				outfiles_ = NULL;
@@ -40,7 +47,7 @@ namespace Cavalia{
 		protected:
 			std::string dir_name_;
 			size_t thread_count_;
-			std::ofstream *outfiles_;
+			FILE **outfiles_;
 		};
 	}
 }
