81f25d53e6f9f7826a63b55f3b3961af1d1074ab
Yingjun <yingjun@comp.nus.edu.sg>
2015-12-10 13:19:05 +0700

    update scripts

--- scripts/tpcc-command-replay.sh
+++ scripts/tpcc-command-replay.sh
@@ -1,6 +1,6 @@
-if [ "$#" -ne 4 ]; then
-	echo "usage: $0 num_warehouse scale_factor num_core dir_name";
+if [ "$#" -ne 2 ]; then
+	echo "usage: $0 num_core dir_name";
 else
-	echo "./tpcc_benchmark -a1 -sf$1 -sf$2 -c$3 -p$4 -r0"
-	./tpcc_benchmark -a1 -sf$1 -sf$2 -c$3 -p$4 -r0
+	echo "./tpcc_benchmark -a1 -c$1 -p$2 -r0"
+	./tpcc_benchmark -a1 -c$1 -p$2 -r0
 fi
