adc8eedbf43ea72df832d6b0023b93b138cd5189
Yingjun Wu <wu.yj0616@gmail.com>
2015-12-15 19:41:48 +0800

    fix bug...

--- Database/Logger/BaseLogger.h
+++ Database/Logger/BaseLogger.h
@@ -140,7 +140,7 @@ namespace Cavalia{
 			}
 
 			// commit command logging.
-			//virtual void CommitTransaction(const size_t &thread_id, const uint64_t &epoch, const uint64_t &commit_ts, const size_t &txn_type, TxnParam *param) = 0;
+			virtual void CommitTransaction(const size_t &thread_id, const uint64_t &epoch, const uint64_t &commit_ts, const size_t &txn_type, TxnParam *param) = 0;
 
 			void CleanUp(const size_t &thread_id){
 				FILE *file_ptr = outfiles_[thread_id];
@@ -164,60 +164,6 @@ namespace Cavalia{
 				ret = fsync(fileno(file_ptr));
 				assert(ret == 0);
 #endif
-			}
-
-
-			// commit command logging.
-			// | param_type | timestamp | param_size | param_content |
-			void CommitTransaction(const size_t &thread_id, const uint64_t &epoch, const uint64_t &commit_ts, const size_t &txn_type, TxnParam *param){
-				ThreadBufferStruct *buf_struct_ptr = thread_buf_structs_[thread_id];
-				size_t &buffer_offset_ref = buf_struct_ptr->buffer_offset_;
-				char *curr_buffer_ptr = buf_struct_ptr->buffer_ptr_ + buffer_offset_ref;
-				// write stored procedure type.
-				memcpy(curr_buffer_ptr, (char*)(&txn_type), sizeof(size_t));
-				// write timestamp.
-				memcpy(curr_buffer_ptr + sizeof(size_t), (char*)(&commit_ts), sizeof(uint64_t));
-				size_t tmp_size = 0;
-				// write parameters. get tmp_size first.
-				param->Serialize(curr_buffer_ptr + sizeof(size_t)+sizeof(uint64_t)+sizeof(size_t), tmp_size);
-				// write parameter size.
-				memcpy(curr_buffer_ptr + sizeof(size_t)+sizeof(uint64_t), (char*)(&tmp_size), sizeof(size_t));
-				buffer_offset_ref += sizeof(size_t)+sizeof(uint64_t)+sizeof(size_t)+tmp_size;
-				assert(buffer_offset_ref < kLogBufferSize);
-
-				if (epoch != buf_struct_ptr->last_epoch_){
-					FILE *file_ptr = outfiles_[thread_id];
-					buf_struct_ptr->last_epoch_ = epoch;
-#if defined(COMPRESSION)
-					//size_t cb_offset = 0;
-					//size_t n = 0;
-					char *compressed_buffer_ptr = buf_struct_ptr->compressed_buffer_ptr_;
-					//auto &context_ref = buf_struct_ptr->compression_context_;
-					//n = LZ4F_compressBegin(context_ref, compressed_buffer_ptr, LZ4_HEADER_SIZE, NULL);
-					//assert(LZ4F_isError(n) == false);
-					//cb_offset += n;
-					size_t bound = LZ4F_compressFrameBound(buffer_offset_ref, NULL);
-					size_t n = LZ4F_compressFrame(compressed_buffer_ptr, bound, buf_struct_ptr->buffer_ptr_, buffer_offset_ref, NULL);
-					assert(LZ4F_isError(n) == false);
-					//cb_offset += n;
-					//n = LZ4F_compressEnd(context_ref, compressed_buffer_ptr + cb_offset, LZ4_FOOTER_SIZE, NULL);
-					//assert(LZ4F_isError(n) == false);
-					//cb_offset += n;
-
-					// after compression, write into file
-					fwrite(compressed_buffer_ptr, sizeof(char), n, file_ptr);
-#else
-					fwrite(curr_buffer_ptr, sizeof(char), buffer_offset_ref, file_ptr);
-#endif
-					buffer_offset_ref = 0;
-					int ret;
-					ret = fflush(file_ptr);
-					assert(ret == 0);
-#if defined(__linux__)
-					ret = fsync(fileno(file_ptr));
-					assert(ret == 0);
-#endif
-				}
 			}
 
 		private:
